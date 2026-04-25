3bd909ad701ef979b20cdb93341868e178595bec
Dmitri Grecov <vokerg@gmail.com>
2026-04-25 20:49:11 +0200

    initial commit

---
+++ src/main/java/com/vokerg/voktrader/VoktraderApplication.java
@@ -0,0 +1,13 @@
+package com.vokerg.voktrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoktraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoktraderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vokerg/voktrader/VoktraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vokerg.voktrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoktraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
