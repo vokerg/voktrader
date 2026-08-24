9ec7f3ef1f4cdc7703c69d38085c78f63ebce02d
vokerg <vokerg@gmail.com>
2026-08-24 07:01:14 +0200

    test(T026): arm scripted live lifecycle fixtures

--- src/test/java/com/vokerg/voktrader/support/ExecutorTestConfig.java
+++ src/test/java/com/vokerg/voktrader/support/ExecutorTestConfig.java
@@ -1,7 +1,12 @@
 package com.vokerg.voktrader.support;
 
 import com.vokerg.voktrader.bot.BotRuntimeManager;
+import com.vokerg.voktrader.executor.ExecutorProperties;
+import com.vokerg.voktrader.trade.LiveArmService;
+import com.vokerg.voktrader.trade.TradingProperties;
+import com.vokerg.voktrader.trade.model.ExecutionMode;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -10,6 +15,8 @@ import static org.mockito.Mockito.mock;
 
 @TestConfiguration
 public class ExecutorTestConfig {
+    private static final String TEST_ACCOUNT_ID = "executor-test-account";
+
     @Bean
     @Primary
     ScriptedExecutorClient scriptedExecutorClient() {
@@ -20,5 +27,23 @@ public class ExecutorTestConfig {
     @Primary
     BotRuntimeManager botRuntimeManager() {
         return mock(BotRuntimeManager.class);
+    }
+
+    @Bean
+    ApplicationRunner armScriptedLiveExecutor(
+            TradingProperties tradingProperties,
+            ExecutorProperties executorProperties,
+            LiveArmService liveArmService
+    ) {
+        return args -> {
+            if (tradingProperties.getMode() != ExecutionMode.LIVE) {
+                return;
+            }
+            executorProperties.setEnabled(true);
+            executorProperties.setDryRun(false);
+            executorProperties.setApiToken("executor-test-token");
+            tradingProperties.setExpectedAccountId(TEST_ACCOUNT_ID);
+            liveArmService.arm(TEST_ACCOUNT_ID);
+        };
     }
 }
